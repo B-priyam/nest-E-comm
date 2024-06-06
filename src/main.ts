707e79ba0cc97e31b19d6072d9de33bf6449af36
B-priyam <Name>
2024-06-06 16:46:54 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule
-  );
+  const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(process.env.PORT ,'0.0.0.0');
+  await app.listen(process.env.PORT);
 }
 bootstrap();
