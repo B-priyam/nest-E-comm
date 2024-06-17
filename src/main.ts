d9f29417268273be923d13eaf6a76c415b9e8357
B-priyam <priyam3801h@gmail.com>
2024-06-17 17:43:46 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // Adjust this to your needs
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  app.use(cors({
+    origin: '*', // Replace with your Next.js frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
   await app.listen(3001);
 }
 bootstrap();
